63d6d4add7ebe0e6649e87b0cff7d90a33e8780d
Rene Hamburger <rene.hamburger@gmail.com>
2020-01-02 10:36:47 +0100

    Refactor XHR wrapper

--- src/xhr-wrapper.ts
+++ src/xhr-wrapper.ts
@@ -1,3 +1,14 @@
+interface XMLHttpRequest {
+  pbb: {
+    request: {
+      method: string;
+      url: string;
+      headers: Record<string, string>,
+      payload?: object
+    }
+  }
+}
+
 const XHR = XMLHttpRequest.prototype;
 const xhrOpen = XHR.open;
 const xhrSend = XHR.send;
@@ -6,38 +17,45 @@ const xhrSetRequestHeader = XHR.setRequestHeader;
 const ROOT = 'https://finanzblick.de/webapp';
 const WATCHED_ROUTES = []; // ['Finance/GetBookings'];
 
-XHR.open = function open(method: string, url: string) {
-  if (WATCHED_ROUTES.some((route) => url === `${ROOT}/${route}`)) {
-    this.pbbRequest = {
+function isWatchedRoute(url: string): boolean {
+  return WATCHED_ROUTES.some((route) => url === `${ROOT}/${route}`)
+}
+
+XHR.open = function open(this: XMLHttpRequest, method: string, url: string) {
+  this.pbb = {
+    request: {
       method,
       url,
       headers: {}
-    };
-  }
+    }
+  };
   return xhrOpen.call(this, method, url);
 };
 
-XHR.setRequestHeader = function setRequestHeader(header: string, value: string) {
-  if (this.pbbRequest) {
-    this.pbbRequest.headers[header] = value;
-  }
+XHR.setRequestHeader = function setRequestHeader(this: XMLHttpRequest, header: string, value: string) {
+  this.pbb.request.headers[header] = value;
   return xhrSetRequestHeader.call(this, header, value);
 };
 
-XHR.send = function send(data) {
-  const request = this.pbbRequest;
-  if (request) {
-    request.data = data;
-    this.addEventListener('load', function onLoad() {
-      const response = {
-        headers: this.getAllResponseHeaders(),
-        body: JSON.parse(this.responseText)
-      };
-      const event = new CustomEvent('fb.xhr', {
-        detail: { request, response }
+XHR.send = function send(this: XMLHttpRequest, body) {
+  const request = this.pbb.request;
+  if (request && typeof body === 'string' && isWatchedRoute(request.url)) {
+    try {
+      const payload = JSON.parse(body);
+      request.payload = payload;
+      this.addEventListener('load', function onLoad() {
+        const response = {
+          headers: this.getAllResponseHeaders(),
+          body: JSON.parse(this.responseText)
+        };
+        const event = new CustomEvent('fb.xhr', {
+          detail: { request, response }
+        });
+        window.dispatchEvent(event);
       });
-      window.dispatchEvent(event);
-    });
+    } catch (err) {
+      console.error(err);
+    }
   }
-  return xhrSend.call(this, data);
+  return xhrSend.call(this, body);
 };
