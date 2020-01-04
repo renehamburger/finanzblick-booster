75d24fadabbe4850d73365f0fdba4c2ae4bbc6c1
Rene Hamburger <rene.hamburger@gmail.com>
2020-01-05 00:13:23 +0100

    Allow for-of loop in eslint
    See https://github.com/airbnb/javascript/issues/1271#issuecomment-281756069
    and whole thread on why it was disallowed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,12 @@ module.exports = {
   extends: ['airbnb-typescript/base'],
   rules: {
     'comma-dangle': [2, 'never'],
-    'linebreak-style': 0
+    'linebreak-style': 0,
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement'
+    ]
   }
 };
