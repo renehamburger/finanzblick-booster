fabfb9ec9d7799e627dedd27eba572d209fb3651
Rene Hamburger <rene.hamburger@gmail.com>
2020-07-15 09:57:25 +0200

    Add source maps for production code
    Sourcemaps are not picked up by Chrome yet.

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  devtool: 'inline-source-map',
-  mode: 'development'
+  mode: 'development',
+  devtool: 'inline-source-map'
 });
