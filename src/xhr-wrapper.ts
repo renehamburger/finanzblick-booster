5f2d0e8777ac0c30d4d63954ea5c7eb403766d3c
Rene Hamburger <rene.hamburger@gmail.com>
2022-06-11 17:07:32 +0200

    Handle all API requests

--- src/xhr-wrapper.ts
+++ src/xhr-wrapper.ts
@@ -1,12 +1,10 @@
-import {
-  PREFIX, FB_API_GET_BOOKINGS_PATH, FB_API_BASE_URL, FB_API_GET_SESSON_INFO_PATH
-} from './shared/const';
+import { PREFIX, FB_API_BASE_URL } from './shared/const';
 import { XHRRequest, XHRResponse } from './shared/interfaces';
 
 interface ExtendedXMLHttpRequest extends XMLHttpRequest {
   pbb: {
-    request: XHRRequest
-  }
+    request: XHRRequest;
+  };
 }
 
 const XHR = XMLHttpRequest.prototype;
@@ -14,10 +12,8 @@ const xhrOpen = XHR.open;
 const xhrSend = XHR.send;
 const xhrSetRequestHeader = XHR.setRequestHeader;
 
-const WATCHED_ROUTES = [FB_API_GET_BOOKINGS_PATH, FB_API_GET_SESSON_INFO_PATH];
-
 function isWatchedRoute(url: string): boolean {
-  return WATCHED_ROUTES.some((route) => url === `${FB_API_BASE_URL}/${route}`);
+  return url.startsWith(FB_API_BASE_URL);
 }
 
 XHR.open = function open(this: ExtendedXMLHttpRequest, method: string, url: string) {
@@ -31,26 +27,32 @@ XHR.open = function open(this: ExtendedXMLHttpRequest, method: string, url: stri
   return xhrOpen.call(this, method, url);
 };
 
-XHR.setRequestHeader = function setRequestHeader(this: ExtendedXMLHttpRequest, header: string,
-  value: string) {
+XHR.setRequestHeader = function setRequestHeader(
+  this: ExtendedXMLHttpRequest,
+  header: string,
+  value: string
+) {
   this.pbb.request.headers[header] = value;
   return xhrSetRequestHeader.call(this, header, value);
 };
 
 XHR.send = function send(this: ExtendedXMLHttpRequest, payload) {
   const { request } = this.pbb;
-  if (request && typeof payload === 'string' && isWatchedRoute(request.url)) {
+  if (request && (payload === null || typeof payload === 'string') && isWatchedRoute(request.url)) {
     try {
-      request.payload = payload;
+      request.payload = payload as any;
       this.addEventListener('load', function onLoad() {
         const response: XHRResponse = {
           headers: this.getAllResponseHeaders(),
           body: this.responseText
         };
-        window.postMessage({
-          action: `${PREFIX}handleXHR`,
-          arguments: [request, response]
-        }, '*');
+        window.postMessage(
+          {
+            action: `${PREFIX}handleXHR`,
+            arguments: [request, response]
+          },
+          '*'
+        );
       });
     } catch (err) {
       // eslint-disable-next-line no-console
